7ffe57ced055de4504158fb56602a46bd71a5dc3
Juan Gómez <gomezj01@stepstone.com>
2024-05-17 13:22:09 -0600

    Card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { Layout } from '../../components/layout'
+import { Card } from '../../components/card'
 
 export const Home = () => {
 	return (
 		<Layout>
 			Home
+			<Card />
 		</Layout>
 	)
 }
