b372bb36ab2026684554ca69a4bd07e2e2d5643b
Juan Gómez <gomezj01@stepstone.com>
2024-05-23 09:59:35 -0600

    OrderCard component

---
+++ src/components/OrderCard/index.jsx
@@ -0,0 +1,21 @@
+import { XMarkIcon } from '@heroicons/react/24/solid'
+import { truncateString } from '../../helpers'
+
+export const OrderCard = ({ product }) => {
+	const { title, image, price } = product
+
+	return (
+		<div className="flex justify-between items-center mb-3">
+			<div className='flex items-center gap-2'>
+				<figure className='w-20 h-20'>
+					<img className='w-full h-full rounded-lg object-cover' src={ image } alt={ title } />
+				</figure>
+				<p className='text-sm font-light '>{ truncateString(title, 50) }</p>
+			</div>
+			<div className='flex items-center gap-2'>
+				<p className='text-lg font-medium'> { price } </p>
+				<XMarkIcon className='h-6 w-6 text-black cursor-pointer' />
+			</div>
+		</div>
+	)
+}
--- src/components/checkoutSideMenu/index.jsx
+++ src/components/checkoutSideMenu/index.jsx
@@ -1,10 +1,11 @@
 import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { OrderCard } from '../OrderCard'
 import { shoppingCartContext } from '../../context'
 import './styles.css'
 
 export const CheckoutSideMenu = () => {
-	const { isCheckouSideMenuOpen, closeCheckoutSideMenuOpen } = useContext(shoppingCartContext)
+	const { isCheckouSideMenuOpen, closeCheckoutSideMenuOpen, productsInCart } = useContext(shoppingCartContext)
 	
 	return (
 		<aside 
@@ -15,6 +16,13 @@ export const CheckoutSideMenu = () => {
 				<div>
 					<XMarkIcon className='size-6 text-black cursor-pointer' onClick={ () => closeCheckoutSideMenuOpen() }/>
 				</div>
+			</div>
+			<div className='px-6'>
+				{
+					productsInCart.map(product => (
+						<OrderCard key={ product.id } product={ product} />
+					))
+				}
 			</div>
 		</aside>
 	)
