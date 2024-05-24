2007b46f0f80c040aa18f854db650d5e62f7ef17
Juan Gómez <gomezj01@stepstone.com>
2024-05-24 14:26:15 -0600

    My Orders

--- src/pages/MyOrder/index.jsx
+++ src/pages/MyOrder/index.jsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
 import { OrderCard } from '../../components/OrderCard'
 import { shoppingCartContext } from '../../context'
 import { Layout } from '../../components/layout'
@@ -8,6 +10,12 @@ export const MyOrder = () => {
 
 	return (
 		<Layout>
+			<div className='flex w-80 items-center relative justify-center mb-6'>
+				<Link to='/my-orders' className='absolute left-0'>
+					<ChevronLeftIcon className='h6 w-6 text-black cursor-pointer'/>
+				</Link>
+				<h1>My Orders</h1>
+			</div>
 			<div className='flex flex-col w-80'>
 				{
 					order?.slice(-1)[0].products.map((product) => (
--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -1,9 +1,28 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { Layout } from '../../components/layout'
+import { shoppingCartContext } from '../../context'
+import { OrdersCard } from '../../components/OrdersCard'
 
 export const MyOrders = () => {
+	const { order } = useContext(shoppingCartContext)
+
 	return (
 		<Layout>
-			MyOrders
+			My Orders
+			{
+				order.map(order => (
+					<Link 
+						key={ crypto.randomUUID() }
+						to={ `/my-orders/${order.id}` }
+					>
+						<OrdersCard 
+							totalPrice={ order.totalPrice }
+							totalProducts={ order.totalProducts }
+						/>
+					</Link>
+				))
+			}
 		</Layout>
 	)
 }
